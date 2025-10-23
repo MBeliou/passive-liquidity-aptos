8b728f9c1f0381ae10c114976602c01ecd8b17d3
MBeliou <maxime.bele@gmail.com>
2025-10-23 17:29:47 +0200

    feat: add sample positions fetching for tapp

--- rust/scrapers/tapp/src/main.rs
+++ rust/scrapers/tapp/src/main.rs
@@ -1,18 +1,49 @@
+mod types;
+
+use anyhow::Result;
 use aptos_rust_sdk::client::builder::AptosClientBuilder;
 use aptos_rust_sdk::client::config::AptosNetwork;
-use anyhow::Result;
+use aptos_rust_sdk_types::api_types::view::ViewRequest;
+use serde_json::Value;
+
+use types::Position;
 
 #[tokio::main]
-async fn main() -> Result<()>{
+async fn main() -> Result<()> {
     println!("Starting Tapp scrapper.");
     let builder = AptosClientBuilder::new(AptosNetwork::mainnet());
     let client = builder.build();
 
+    let view_address = "0xf5840b576a3a6a42464814bc32ae1160c50456fb885c62be389b817e75b2a385";
+
     //AptosClientBuilder::ne
-    
+
     let state = client.get_state().await?;
 
     println!("State: {:?}", state);
+
+    let query = client
+        .view_function(ViewRequest {
+            arguments: vec![
+                Value::String(
+                    "0x4ed8fda291b604491ead0cc9e5232bc1edc1f31d0e0cf343be043d8c792af1a8"
+                        .to_string(), //"0x8f9dd0b94f1c18f96c00dfbff8254f20304f05713bb841d20b203f9446a75b9".to_string(),
+                ),
+                //Value::Number(10.into()),
+            ],
+            function: format!("{}::clmm_views::get_positions", view_address),
+            type_arguments: vec![],
+        })
+        .await?;
+    
+    let response = query.into_inner();
+    // First, print to see the structure
+println!("Raw response: {}", serde_json::to_string_pretty(&response)?);
+
+
+let positions: Vec<Position> = serde_json::from_value(response[0].clone())?;
+
+    println!("Positions: {:?}", positions);
 
     Ok(())
 }
---
+++ rust/scrapers/tapp/src/types.rs
@@ -0,0 +1,49 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TickIndex {
+    pub bits: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Position {
+    pub fee_growth_inside_a: String,
+    pub fee_growth_inside_b: String,
+    pub fee_owed_a: String,
+    pub fee_owed_b: String,
+    pub index: String,
+    pub liquidity: String,
+    pub tick_lower_index: TickIndex,
+    pub tick_upper_index: TickIndex,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TappApiApr {
+    #[serde(rename = "boostedAprPercentage")]
+    pub boosted_apr_percentage: f64,
+    #[serde(rename = "campaignAprs")]
+    pub campaign_aprs: Vec<CampaignApr>,
+    #[serde(rename = "feeAprPercentage")]
+    pub fee_apr_percentage: f64,
+    #[serde(rename = "totalAprPercentage")]
+    pub total_apr_percentage: f64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CampaignApr {
+    #[serde(rename = "aprPercentage")]
+    pub apr_percentage: f64,
+    #[serde(rename = "campaignIdx")]
+    pub campaign_idx: u32,
+    pub token: TappApiToken,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TappApiToken {
+    pub addr: String,
+    pub color: String,
+    pub decimals: u8,
+    pub img: String,
+    pub symbol: String,
+    pub verified: bool,
+}
