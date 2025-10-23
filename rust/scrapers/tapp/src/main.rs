20f8ba08ee438f04facf5415fa0be60f37a4213d
MBeliou <maxime.bele@gmail.com>
2025-10-23 16:04:54 +0200

    feat: Add basic aptos client for tapp

--- rust/scrapers/tapp/src/main.rs
+++ rust/scrapers/tapp/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use aptos_rust_sdk::client::builder::AptosClientBuilder;
+use aptos_rust_sdk::client::config::AptosNetwork;
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    println!("Starting Tapp scrapper.");
+    let builder = AptosClientBuilder::new(AptosNetwork::mainnet());
+    let client = builder.build();
+
+    //AptosClientBuilder::ne
+    
+    let state = client.get_state().await?;
+
+    println!("State: {:?}", state);
+
+    Ok(())
 }
