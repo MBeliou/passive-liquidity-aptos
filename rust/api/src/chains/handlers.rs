5a5cf775dbd5f954f46a140b76d8ebce24d5d62d
MBeliou <maxime.bele@gmail.com>
2025-12-17 15:00:08 +0100

    API: add chain/{id} stub

--- rust/api/src/chains/handlers.rs
+++ rust/api/src/chains/handlers.rs
@@ -2,6 +2,7 @@ use crate::{errors::AppResult, models::chain};
 use axum::{Json, extract::State};
 
 pub async fn list_chains() -> AppResult<Json<chain::ChainsResponse>> {
+    // TODO: stub
     let chains = vec![chain::Chain {
         name: "Aptos".to_string(),
         id: "aptos".to_string(),
@@ -10,4 +11,12 @@ pub async fn list_chains() -> AppResult<Json<chain::ChainsResponse>> {
     let count = 1;
 
     Ok(Json(chain::ChainsResponse { chains, count }))
+}
+
+pub async fn get_chain(id: String) -> AppResult<Json<chain::Chain>> {
+    // TODO: stub
+    Ok(Json(chain::Chain {
+        name: "Aptos".to_string(),
+        id: "aptos".to_string(),
+    }))
 }
--- rust/api/src/chains/mod.rs
+++ rust/api/src/chains/mod.rs
@@ -1,9 +1,11 @@
 mod handlers;
 
-use std::sync::Arc;
-use axum::{Router, routing::get};
 use crate::AppState;
+use axum::{Router, routing::get};
+use std::sync::Arc;
 
 pub fn router() -> Router<Arc<AppState>> {
-    Router::new().route("/chains", get(handlers::list_chains))
+    Router::new()
+        .route("/chains", get(handlers::list_chains))
+        .route("/chains/{id}", get(handlers::get_chain))
 }
